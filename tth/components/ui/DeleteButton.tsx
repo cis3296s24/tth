025e00e0444a14fc9509036b7818f0a65b94136a
tuh25772 <143642574+tuh25772@users.noreply.github.com>
2024-04-09 19:17:40 +0600

    Add Post Delete Feature

---
+++ tth/components/ui/DeleteButton.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useRouter } from "next/navigation";
+import { deleteDoc, doc, getFirestore } from "firebase/firestore";
+
+interface Items {
+  id: string;
+  title: string;
+  link: string;
+  tag: string;
+  description: string;
+  user_id: string;
+}
+function DeleteButton({ projectID, currentUserID }: { projectID: Items; currentUserID: string }) {
+    const fireDB = getFirestore();
+    const router = useRouter();
+
+    const deleteItemes=async()=>{
+         try {
+         console.log(projectID)
+        if(projectID.user_id === currentUserID){
+      // Get reference to the document
+      const docRef = doc(fireDB, 'Item', projectID.id);
+      // Delete the document
+      await deleteDoc(docRef);
+  
+      console.log('Document successfully deleted!');
+      router.push('/fees');
+       }
+
+        else{
+          console.log('You do not have permission to delete it');
+        }
+
+
+    } catch (error) {
+      console.error('Error deleting document:', error);
+    }
+
+    };
+  return (
+    <>
+    {projectID.user_id === currentUserID &&
+    <button  onClick={deleteItemes} 
+    className="bg-red-500 hover:bg-red-600 text-white text-sm py-1 px-2 rounded">Delete</button>
+    }
+   
+    </>
+  )
+}
+
+export default DeleteButton
